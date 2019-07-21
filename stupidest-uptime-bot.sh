30d5d6a875dcbf9af765eb0258df146a61747295
Niels Saurer <me@nielssaurer.com>
2019-07-21 15:34:44 +0200

    Working version

--- stupidest-uptime-bot.sh
+++ stupidest-uptime-bot.sh
@@ -1,1 +1,18 @@
-# first commit
+#!/bin/bash
+
+source "$(dirname "$0")/config.sh"
+
+check_port () {
+    port=$1
+    curl -f -m $timeout "localhost:$port"
+    if [ $? -ne 0 ]
+    then
+        echo "Failed: $port"
+        eval "${services[$port]}"
+    fi
+}
+
+for port in "${!services[@]}" 
+do
+    check_port $port
+done
